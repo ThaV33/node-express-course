939b28b5b133fa6cd2d9b40100d7d33d804e0f54
vee <iam.vee01@gmail.com>
2021-03-23 16:08:03 +0100

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express'); //gives access to the express library by searching my node_modules for 'express'.
 const app = express(); //creates an instance of the express constructor, named 'app'.
 
+const mockUserDate = [
+    { name: 'Victor' },
+    { name: 'Abolarin' }
+]
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'Successfully got users, Nice!',
+        users: mockUserDate
+    })
+})
+
+
+
 app.listen(8000, function() {
     console.log("Server is Running")
 })

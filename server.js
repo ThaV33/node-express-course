7fffe5a715bd729ba1e17a805f90e7ef4b1428b5
vee <iam.vee01@gmail.com>
2021-03-25 17:42:16 +0100

    add login post route

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express'); //gives access to the express library by searching my node_modules for 'express'.
 const app = express(); //creates an instance of the express constructor, named 'app'.
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+
+//GET REQUEST
 const mockUserDate = [
-        { name: 'Victor' },
-        { name: 'Abolarin' }
-    ]
-    // app.get('/users', function(req, res) {
-    //     res.json({
-    //         success: true,
-    //         message: 'Successfully got users, Nice!',
-    //         users: mockUserDate
-    //     })
-    // })
+    { name: 'Victor' },
+    { name: 'Abolarin' }
+]
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'Successfully got users, Nice!',
+        users: mockUserDate
+    })
+})
+
 
 app.get('/users/:id', function(req, res) {
     console.log(req.params.id)
@@ -21,6 +27,36 @@ app.get('/users/:id', function(req, res) {
         user: req.params.id
     })
 })
+
+
+//POST REQUEST
+app.post('/login', function(req, res)) {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //This should come from database
+    const mockUsername = "thav33";
+    const mockPassword = "mypassword";
+
+    if (username === mockUsername && password === mockPassword) {
+        // In practice, use JSON web token sign method here to make an encrypted token
+        res.join({
+            success: true,
+            message: 'PASSWORD AND USERNAME MATCH!',
+            token: 'encrypted token goes here'
+
+        })
+
+    } else {
+        res.json({
+            success: false,
+            message: 'PASSWORD AND USERNAME MATCH!'
+        })
+    }
+
+
+}
 
 
 

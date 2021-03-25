843d236bd8dafda6593c8707123627236c77faf7
vee <iam.vee01@gmail.com>
2021-03-25 09:40:03 +0100

    add dynamic GET route

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express'); //gives access to the express library by sea
 const app = express(); //creates an instance of the express constructor, named 'app'.
 
 const mockUserDate = [
-    { name: 'Victor' },
-    { name: 'Abolarin' }
-]
-app.get('/users', function(req, res) {
+        { name: 'Victor' },
+        { name: 'Abolarin' }
+    ]
+    // app.get('/users', function(req, res) {
+    //     res.json({
+    //         success: true,
+    //         message: 'Successfully got users, Nice!',
+    //         users: mockUserDate
+    //     })
+    // })
+
+app.get('/users/:id', function(req, res) {
+    console.log(req.params.id)
     res.json({
         success: true,
-        message: 'Successfully got users, Nice!',
-        users: mockUserDate
+        message: 'got one user',
+        user: req.params.id
     })
 })
+
+
 
 
 
